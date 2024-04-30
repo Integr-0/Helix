aa12dada76f860998d7563ac8bc683cbc15eee38
Integr-0 <erik.reitbauer@outlook.de>
2024-04-30 15:36:27 +0200

    Readme

--- src/main/kotlin/net/integr/utilities/CommandSender.kt
+++ src/main/kotlin/net/integr/utilities/CommandSender.kt
@@ -1,21 +1,11 @@
 package net.integr.utilities
 
 import net.integr.Helix
-import net.minecraft.network.encryption.NetworkEncryptionUtils
-import net.minecraft.network.message.LastSeenMessagesCollector
-import net.minecraft.network.message.LastSeenMessagesCollector.LastSeenMessages
-import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket
-import java.time.Instant
-
 
 class CommandSender {
     companion object {
         fun sendChatCommand(command: String?) {
-            val instant: Instant = Instant.now()
-            val l = NetworkEncryptionUtils.SecureRandomUtil.nextLong()
-            val lastSeenMessages: LastSeenMessages = LastSeenMessagesCollector(0).collect()
-
-            Helix.MC.networkHandler!!.sendPacket(CommandExecutionC2SPacket(command, instant, l, null, lastSeenMessages.update))
+            Helix.MC.networkHandler!!.sendChatCommand(command)
         }
     }
 }
